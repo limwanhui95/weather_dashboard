52abc35bd35adf3759e0a8e4ba1eb5227c0295be
unknown <limwanhui@gmail.com>
2024-02-27 14:07:36 +0800

    1st push, show current weather temperature, wind, humidity, uv index, location, time and last update time

---
+++ index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+import Appip from "apiip.net"
+
+const weatherURL = "http://api.weatherapi.com/v1";
+
+const app = express();
+const port = 3000;
+const weather_apiKey = "365759bb0ea8432b88690535242602";
+const appip_apiKey = "aa71965c-89fa-4b15-a950-26c156764bbe";
+const appip = Appip(appip_apiKey);
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+// render homepage
+app.get("/",async(req,res)=>{
+    try{
+        var geoResult = await appip.getLocation();
+        var weatherResult = await axios.get(weatherURL+`/current.json?key=${weather_apiKey}&q=${geoResult.latitude}, ${geoResult.longitude}&aqi=yes`);
+        var forecastResult = await axios.get(weatherURL+`/forecast.json?key=${weather_apiKey}&q=${geoResult.latitude}, ${geoResult.longitude}`);
+        console.log(geoResult);
+        console.log(weatherResult.data);
+        
+        res.render("index.ejs", { 
+            geoData: geoResult,
+            weatherData: weatherResult.data,
+            weatherImgSrc: getImgPath(weatherResult.data) 
+        });
+        // res.render("index.ejs");
+    } catch (error) {
+        console.log("error");
+        console.error(error);
+    }
+});
+
+
+app.listen(port,()=>{
+    console.log(`Server running on port: ${port}`);
+});
+
+function getImgPath(data) {
+    let imgPath = data.current.condition.icon;
+    let imgPathArray = imgPath.split("/");
+    let imgSrc = `images/weather/${imgPathArray[imgPathArray.length-2]}/${imgPathArray[imgPathArray.length-1]}`;
+    return imgSrc;
+}
